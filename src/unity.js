04fb16ba45c45bf01376c9f3449c0ab4eb1372d1
Tiago Santos <tiago@ticogame.com>
2022-10-10 11:54:54 -0300

    Update unity.js
    changing or to and

--- src/unity.js
+++ src/unity.js
@@ -19,7 +19,7 @@ async function createManualActivationFile(unityPath) {
 async function activateManualLicense(unityPath, licenseData) {
     fs.writeFileSync('license.ulf', licenseData);
     const stdout = await executeUnity(unityPath, `-batchmode -nographics -quit -logFile "-" -manualLicenseFile license.ulf`);
-    if (!stdout.includes('Next license update check is after') || !stdout.includes('Successfully processed offline license activation request')) {
+    if (!stdout.includes('Next license update check is after') && !stdout.includes('Successfully processed offline license activation request')) {
         throw new Error('Activation failed');
     }
 }
